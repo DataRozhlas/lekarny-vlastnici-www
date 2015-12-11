37d0bc918c398f86f0cda8a08a352927e45709f5
Jan Cibulka <honza@datastory.cz>
2015-12-11 13:45:15 +0100

    Upraveno vypisování podrobností o lékárně.

--- base.js
+++ base.js
@@ -13,6 +13,24 @@ var style = function (retezec) {
     };
     style.layers = [
     {
+      "id": "nezavisle",
+      "interactive": true,
+      "type": "circle",
+      "source": "lekarny",
+      "visibility": (retezec == "nezavisle" || retezec == "any" ? "visible": "none"),
+      "source-layer": "lekarny",
+      "filter": [
+        "all",
+        ["!in", "znacka_2015", "Agel", "BENU", "Devětsil JST", "Dr.Max", "Plzeňská lékárna"],
+        ],
+      "paint": {
+        "circle-color": "#999999",
+        "circle-radius": 4,
+        //"fill-outline-color": "#d9d9d9",
+        "circle-opacity": 0.8
+    }
+    },
+    {
       "id": "agel",
       "interactive": true,
       "type": "circle",
@@ -25,7 +43,7 @@ var style = function (retezec) {
         ],
       "paint": {
         "circle-color": "#80b1d3",
-        "circle-radius": 3,
+        "circle-radius": 4,
         //"fill-outline-color": "#d9d9d9",
         "circle-opacity": 0.8
     }
@@ -36,14 +54,14 @@ var style = function (retezec) {
       "type": "circle",
       "source": "lekarny",
       "source-layer": "lekarny",
-      "visibility": (retezec == "Benu" || retezec == "any" ? "visible": "none"),
+      "visibility": (retezec == "BENU" || retezec == "any" ? "visible": "none"),
       "filter": [
         "all",
-        ["==", "znacka_2015", "Benu"],
+        ["==", "znacka_2015", "BENU"],
         ],
       "paint": {
         "circle-color": "#b3de69",
-        "circle-radius": 3,
+        "circle-radius": 4,
         //"fill-outline-color": "#d9d9d9",
         "circle-opacity": 0.8
     }
@@ -61,25 +79,7 @@ var style = function (retezec) {
     	"visibility": (retezec == "Devětsil JST" || retezec == "any" ? "visible": "none"),
       "paint": {
         "circle-color": "#bebada",
-        "circle-radius": 3,
-        //"fill-outline-color": "#d9d9d9",
-        "circle-opacity": 0.8
-    }
-    },
-    {
-      "id": "drmax",
-      "interactive": true,
-      "type": "circle",
-      "source": "lekarny",
-      "source-layer": "lekarny",
-      "visibility": (retezec == "Dr.Max" || retezec == "any" ? "visible": "none"),
-      "filter": [
-        "all",
-        ["==", "znacka_2015", "Dr.Max"],
-        ],
-      "paint": {
-        "circle-color": "#fb8072",
-        "circle-radius": 3,
+        "circle-radius": 4,
         //"fill-outline-color": "#d9d9d9",
         "circle-opacity": 0.8
     }
@@ -97,25 +97,25 @@ var style = function (retezec) {
         ],
       "paint": {
         "circle-color": "#fdb462",
-        "circle-radius": 3,
+        "circle-radius": 4,
         //"fill-outline-color": "#d9d9d9",
         "circle-opacity": 0.8
     }
     },
     {
-      "id": "nezavisle",
+      "id": "drmax",
       "interactive": true,
       "type": "circle",
       "source": "lekarny",
-      "visibility": (retezec == "nezavisle" || retezec == "any" ? "visible": "none"),
       "source-layer": "lekarny",
+      "visibility": (retezec == "Dr.Max" || retezec == "any" ? "visible": "none"),
       "filter": [
         "all",
-        ["==", "znacka_2015", ""],
+        ["==", "znacka_2015", "Dr.Max"],
         ],
       "paint": {
-        "circle-color": "#999999",
-        "circle-radius": 3,
+        "circle-color": "#fb8072",
+        "circle-radius": 4,
         //"fill-outline-color": "#d9d9d9",
         "circle-opacity": 0.8
     }
@@ -124,8 +124,19 @@ var style = function (retezec) {
     style.layers =  style.layers.filter(function (s){
       return s.visibility !== "none"
     });
-    console.log(style.layers);
     return style
+};
+
+var timeBack = function(zmena, n07, n99) {
+  if (zmena == "n") {
+    return "";
+  } else if (n07 != null) {
+    return "Původní název " + n07;
+  } else if (n99 != null) {
+    return "Původní název " + n99;
+  } else {
+    return "Lékárna v minulosti změnila vlastníka, název neznáme.";
+  }
 };
 
 
@@ -143,16 +154,15 @@ var drawMap = function(retezec) {
 	//map.addControl(new mapboxgl.Navigation());
 
 	map.on('mousemove', function(e) {
-	    map.featuresAt(e.point, {}, function(err, features) {
+	    map.featuresAt(e.point, {radius: 5}, function(err, features) {
 	        if (err) throw err;
 	        $(".info").empty()
-	        $("#chart").empty()
-	        $(".info").append("Honitba " + features[0].properties['NAZEV'] + "<br>" + zverNazvy[zvire.replace('JKS_', '').replace('_STAV', '').replace('_pct', '')] + ": " + features[0].properties[zvire.substr(0, zvire.length - 4)])
+	        $(".info").append("<b>" + features[0].properties.nazev_2015 + "</b><br>Adresa: <b>" + features[0].properties.adresa + "</b><br>" + timeBack(features[0].properties.zmena, features[0].properties.nazev_2007, features[0].properties.nazev_1999))
 	    });
 	});
 };
 
-drawMap("Benu");
+drawMap("any");
 
 $(".selector").change(function(evt) {
     drawMap(evt.target.value)
